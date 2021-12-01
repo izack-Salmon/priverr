7a8d7759ba562133e3de96042389e87d0686649d
Revital Elishkov <revitalrle@gmail.com>
2021-12-01 12:48:16 +0200

    add templates in list & preview

---
+++ src/cmps/gig-list.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="gig-list">
+    <div v-for="gig in gigs" :key="gig._id" >
+        <gig-preview :gig="gig"></gig-preview>
+    </div>
+  </section>
+</template>
+
+<script>
+import gigPreview from "../cmps/gig-preview.vue";
+
+export default {
+  name: gigList,
+   props: {
+    gigs: Array,
+  },
+  components:{
+    gigPreview,
+  }
+};
+</script>
