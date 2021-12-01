5e0d74c2e8d5f4eab95969ee2a274aa5a64e67a0
izack salomon <hmje123123123@gmail.com>
2021-12-01 20:46:17 +0200

    8:40 wenstday

--- src/views/gig-details.vue
+++ src/views/gig-details.vue
@@ -1,42 +1,42 @@
 <template>
   <section class="gig-details">
     <h1>gig details</h1>
-    <div class="gig-details-container">
-      <h1>{{ gig.title }}</h1>
+    <div class="gig-details-container"></div>
+    <h1>{{ gig.title }}</h1>
 
+    <div class="seller-overview">
+      <img src="gig.owner.imgUrl" />
+      <div>
+        <a href="">{{ gig.owner.fullname }}</a>
+        <span>level 3 seller</span>
+      </div>
+
+      <div>
+        <div class="gig-rating">
+          <span>⭐4.8</span>
+          <span>(1k+)</span>
+        </div>
+        <span> 80 Orders in Queue </span>
+      </div>
+    </div>
+    <div>img carussle</div>
+    <h2>What people loved about this seller</h2>
+    <a href="">See all reviews</a>
+    <div>review carussle</div>
+    <div>
+      <h2>About This Gig</h2>
+      <p>{{ gig.description }}</p>
+    </div>
+    <div>
+      <h2>About The Seller</h2>
       <div class="seller-overview">
         <img src="gig.owner.imgUrl" />
         <div>
           <a href="">{{ gig.owner.fullname }}</a>
-          <span>level 3 seller</span>
-        </div>
-
-        <div>
-          <div class="gig-rating">
-            <span>⭐4.8</span>
-            <span>(1k+)</span>
-          </div>
-          <span> 80 Orders in Queue </span>
-        </div>
-      </div>
-      <div>img carussle</div>
-      <h2>What people loved about this seller</h2>
-      <a href="">See all reviews</a>
-      <div>review carussle</div>
-      <div>
-        <h2>About This Gig</h2>
-        <p>{{ gig.description }}</p>
-      </div>
-      <div>
-        <h2>About The Seller</h2>
-        <div class="seller-overview">
-        <img src="gig.owner.imgUrl" />
-        <div>
-          <a href="">{{ gig.owner.fullname }}</a>
           <p>Professional Logo Designer</p>
-            <span>⭐4.8</span>
-            <span>(1k+)</span>
-            <button>Contact Me</button>
+          <span>⭐4.8</span>
+          <span>(1k+)</span>
+          <button>Contact Me</button>
         </div>
         <div>
           <ul>
@@ -48,14 +48,14 @@
         </div>
       </div>
     </div>
-    <gig-purchase :gig="gig" />
+    <!-- <gig-purchase :gig="gig" /> -->
   </section>
 </template>
 
 <script>
-import gigPurchase from "../cmps/gig-purchase.vue";
+// import gigPurchase from "../cmps/gig-purchase.vue";
 export default {
-  components: { gigPurchase },
+  // components: { gigPurchase },
   name: "gigDetails",
   created() {
     this.getGigById();
