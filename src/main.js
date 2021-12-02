210aac1439cb7ddf1c88430fe46a4ede12157fe1
izack salomon <hmje123123123@gmail.com>
2021-12-02 09:58:55 +0200

    10:00

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import './styles/styles.scss'
-import $ from 'jquery'
 import VueCarousel from 'vue-carousel';
 
 
