f993014cb0037d1af89c4e463db9a99d1c606bd8
Revital Elishkov <revitalrle@gmail.com>
2021-12-08 16:40:14 +0200

    start seller gigs

---
+++ front/src/cmps/seller-gigs.vue
@@ -0,0 +1,34 @@
+
+<template>
+ <section>
+     <h3>Active Gigs</h3>
+    <div v-for="(gig, idx) in gigs" :key="idx">
+      <div class="gig-seller-card">
+          <img class="gig-img" :src="gig.imgUrl[0]" contextmenu=""/>
+      
+         <p class="gig-seller-title" >
+           {{ gig.title }}
+         </p>
+
+      <div class="gig-seller btns flex space-between">
+        <button></button>
+
+        <router-link :to=" + gig._id">
+          <span class="edit-btn"> Edit </span>
+        </router-link>
+      </div>
+    </div>
+ </div>
+   
+ </section>
+</template>
+
+<script>
+export default {
+    name: 'sellerGigs',
+    props: {
+    gigs: Array
+    },
+
+}
+</script>
