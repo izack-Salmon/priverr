b947847b7ae8aad892683cb8259f9444ee66a9e7
Revital Elishkov <revitalrle@gmail.com>
2021-12-09 09:52:29 +0200

    fix filter add seller gig

--- front/src/services/gig.service.js
+++ front/src/services/gig.service.js
@@ -73,28 +73,17 @@ async function _createGigs() {
     }
     return gigs;
 }
-<<<<<<< HEAD
 function _createGig(imgUrls,label,title, user,) {
     const levels= ['Level 2 Seller','Level 1 Seller','Top Rated Seller']
     const rates = [4.7,4.8,4.9,4.6,4.5, ]
-=======
-function _createGig(imgUrls, label, title, user,) {
-    const levels = ['Level 2 Seller', 'Level 1 Seller', 'Top Rated Seller']
-
->>>>>>> 10f3b2eeba95622bb8312fb82e75879a6f324fba
     console.log('user-gig-serv', user);
     var { _id, fullname, imgUrl } = user
     const miniUser = {
         _id,
         fullname,
         imgUrl,
-<<<<<<< HEAD
         rate: rates[utilService.getRandomInt(0, levels.length-1)],
         level: levels[utilService.getRandomInt(0, levels.length-1)],
-=======
-        rate: Math.round(Math.random * utilService.getRandomInt(4, 5) * 10) / 10,
-        level: levels[utilService.getRandomInt(0, levels.length - 1)],
->>>>>>> 10f3b2eeba95622bb8312fb82e75879a6f324fba
         orderNum: utilService.getRandomInt(100, 1000)
     }
     // console.log(miniUser);
